6dc55228c87d9b3000a47fc1f82395e71f6ea28f
daniel30-07 <danielborgesdecarvalho@gmail.com>
2021-03-08 22:54:13 -0300

    First commit - Enviromental config

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./styles/global.scss";
+
+export function App() {
+  //throw new Error("Eita Porra");
+
+  return <h1>Hello World!!</h1>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/, //verifica se o arquivo é .jsx
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/, //verifica se o arquivo é .jsx
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
